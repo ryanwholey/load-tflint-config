d63537a036c6028be0beab1dc15eb19723f1c97f
Ryan Wholey <ryan@takescoop.com>
2021-06-22 11:21:50 -0600

    try different output

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,3 +10,5 @@ fi
 
 git clone --single-branch --branch "$INPUT_SOURCE_REPO_BRANCH" "https://${auth}github.com/${INPUT_SOURCE_REPO}.git" "${INPUT_CLONE_DIRECTORY_ROOT}/${INPUT_SOURCE_REPO}"
 cp "${INPUT_CLONE_DIRECTORY_ROOT}/${INPUT_SOURCE_REPO}/${INPUT_SOURCE_PATH}" $INPUT_DESTINATION_PATH
+
+echo "::set-output name=path::${INPUT_DESTINATION_PATH}"
