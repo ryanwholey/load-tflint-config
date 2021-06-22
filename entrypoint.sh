aee4bcb7c1e6ce1f2012c033976a1790ba3096c5
Ryan Wholey <ryan@takescoop.com>
2021-06-22 10:55:29 -0600

    Init

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -ex
+
+if [ -z "$INPUT_TOKEN" ] ; then
+  auth=""
+else 
+  auth="x-access-token:${INPUT_TOKEN}@"
+fi
+
+git clone --single-branch --branch "$INPUT_SOURCE_REPO_BRANCH" "https://${auth}github.com/${INPUT_SOURCE_REPO}.git" "${INPUT_CLONE_DIRECTORY_ROOT}/${INPUT_SOURCE_REPO}"
+cp "${INPUT_CLONE_DIR_ROOT}/${INPUT_SOURCE_REPO}/${INPUT_SOURCE_PATH}" $INPUT_DESTINATION_PATH
